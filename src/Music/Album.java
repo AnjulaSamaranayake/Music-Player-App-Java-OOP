406198478aa0d86f551ba12c8bc81ae070d03640
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2025-03-13 11:00:22 +0530

    added two albums
    i checked song add part and its work properly

--- src/Music/Album.java
+++ src/Music/Album.java
@@ -8,10 +8,10 @@ public class Album {
     private String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
+    public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = songs;
+        this.songs = new ArrayList<Song>();
     }
 
     public Album() {
@@ -29,24 +29,35 @@ public class Album {
     public boolean addSong(String title, double duration) {
         if(findSong(title) == null){
             songs.add(new Song(title, duration));
-            System.out.println(title + "succesfully added to the list");
+            System.out.println(title + " succesfully added to the list");
             return true;
         }
         else {
-            System.out.println(title + "song already exists in the list");
+            System.out.println(title + " song already exists in the list");
             return false;
         }
     }
 
     public boolean addToPlayList(int trackNumber, LinkedList<Song> Playlist) {
         int index = trackNumber - 1;
-        if (index > 0 && index <= songs.size()) {
+        if (index > 0 && index <= this.songs.size()) {
             Playlist.add(this.songs.get(index));
             return true;
         }
-        else {
-            System.out.println("this album does not have song with track number " + trackNumber);
-            return false;
-        }
+        System.out.println("this album does not have song with track number " + trackNumber);
+        return false;
     }
+
+    //method overloading
+    public boolean addToPlayList(String title, LinkedList<Song> Playlist) {
+        for (Song checkSong : this.songs) {
+            if (checkSong.getTitle().equals(title)) {
+                Playlist.add(checkSong);
+                return true;
+            }
+        }
+        System.out.println("this album does not have song with title " + title);
+        return false;
+    }
+
 }
--- src/Music/Main.java
+++ src/Music/Main.java
@@ -1,7 +1,29 @@
 package Music;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
+
     public static void main(String[] args) {
 
+        Album album = new Album("Kalunika","Smokio");
+
+        album.addSong("Kalunika",3.0);
+        album.addSong("Dushtaya",4.0);
+        album.addSong("Awasana Washayen",3.5);
+        album.addSong("Yeheli",3.2);
+        albums.add(album);
+
+        album = new Album("Neththara","BNS");
+
+        album.addSong("Neththara",3.0);
+        album.addSong("Kiri Kodu",4.2);
+        album.addSong("Chandani Payala",3.6);
+        album.addSong("Mana Bandu",3.5);
+        albums.add(album);
+
+        
     }
 }
